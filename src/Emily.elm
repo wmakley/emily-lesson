b5f94d6f9a6acdb3d71ea9632e340288cc4c50c6
William Makley <wmakley@tortus.com>
2019-01-09 21:12:46 -0500

    initial commit

---
+++ src/Emily.elm
@@ -0,0 +1,98 @@
+module Emily exposing (main)
+
+import Browser
+import Html
+import Html.Attributes
+import Html.Events
+
+
+main : Program () Model Msg
+main =
+    Browser.sandbox
+        { init = init
+        , update = update
+        , view = view
+        }
+
+
+type alias Model =
+    { answer : String
+    , answerShown : Bool
+    }
+
+
+init : Model
+init =
+    { answer = "a delightful teacher!"
+    , answerShown = False
+    }
+
+
+type Msg
+    = ShowAnswer
+    | HideAnswer
+    | ChangeAnswer String
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        ShowAnswer ->
+            { model | answerShown = True }
+
+        HideAnswer ->
+            { model | answerShown = False }
+
+        ChangeAnswer newAnswer ->
+            { model | answer = newAnswer }
+
+
+view : Model -> Html.Html Msg
+view model =
+    Html.div []
+        [ Html.h1
+            [ Html.Attributes.style "color"
+                (if model.answerShown then
+                    "green"
+
+                 else
+                    "orange"
+                )
+            , Html.Attributes.style "text-decoration"
+                (if model.answerShown then
+                    "none"
+
+                 else
+                    "underline"
+                )
+            , Html.Attributes.style "font-style"
+                (if model.answerShown then
+                    "normal"
+
+                 else
+                    "italic"
+                )
+            ]
+            [ Html.text "Hi! William is "
+            , if model.answerShown then
+                Html.text model.answer
+
+              else
+                Html.button
+                    [ Html.Events.onClick ShowAnswer ]
+                    [ Html.text "Show Answer  " ]
+            , Html.text " "
+            , Html.button
+                [ Html.Events.onClick HideAnswer ]
+                [ Html.text "Hide Answer" ]
+            ]
+        , Html.p
+            []
+            [ Html.input
+                [ Html.Attributes.type_ "text"
+                , Html.Attributes.value model.answer
+                , Html.Events.onInput ChangeAnswer
+                ]
+                []
+            ]
+        ]
